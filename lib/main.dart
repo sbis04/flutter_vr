1a45c12d9fdfc03aa156400e897b488e08447c6a
Souvik Biswas <sbis1999@gmail.com>
2019-10-11 13:57:21 +0530

    Updated to flutter_custom_tabs

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
+import 'package:flutter_custom_tabs/flutter_custom_tabs.dart';
 
 void main() {
   SystemChrome.setEnabledSystemUIOverlays([]);
@@ -17,11 +17,72 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      routes: {
-        "/": (_) => WebviewScaffold(
-              url: "https://sbis04.github.io/demo360",
-            ),
-      },
+      title: 'Flutter VR',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Container(
+          color: Colors.blue,
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Row(),
+              Text(
+                'Flutter VR',
+                style: TextStyle(
+                  fontSize: 60,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold
+                ),
+              ),
+              SizedBox(height: 20),
+              RaisedButton(
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30)),
+                color: Colors.blue[800],
+                child: Padding(
+                  padding: const EdgeInsets.all(10),
+                  child: Text(
+                    'ENTER',
+                    style: TextStyle(
+                      fontSize: 25,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+                onPressed: () => _launchURL(context),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
+  }
+
+  Future<void> _launchURL(BuildContext context) async {
+    try {
+      await launch(
+        'https://sbis04.github.io/demo360',
+        option: CustomTabsOption(
+          toolbarColor: Theme.of(context).primaryColor,
+          enableDefaultShare: true,
+          enableUrlBarHiding: true,
+          showPageTitle: false,
+          animation: CustomTabsAnimation.slideIn(),
+          extraCustomTabs: const <String>[
+            // ref. https://play.google.com/store/apps/details?id=org.mozilla.firefox
+            'org.mozilla.firefox',
+            // ref. https://play.google.com/store/apps/details?id=com.microsoft.emmx
+            'com.microsoft.emmx',
+          ],
+        ),
+      );
+    } catch (e) {
+      // An exception is thrown if browser app is not installed on Android device.
+      debugPrint(e.toString());
+    }
   }
 }
